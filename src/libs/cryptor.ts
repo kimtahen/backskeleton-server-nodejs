29ad7b457037bb4d56e198e860c416f531cefca7
chunghwanyoon <chunghwan@redrocket.co.kr>
2020-08-12 22:16:44 +0900

    feat: add Cryptor class to hashing password

---
+++ src/libs/cryptor.ts
@@ -0,0 +1,13 @@
+import * as crypto from 'crypto';
+import 'dotenv/config';
+
+export class Cryptor {
+  private HASING_KEY = process.env.HASHING_KEY;
+  private ALGORITHM = 'sha1';
+
+  public async encrypt(payload: string): Promise<string> {
+    return crypto.createHmac(this.ALGORITHM, this.HASING_KEY).update(payload).digest('base64');
+  }
+
+  public async decrypt() {}
+}
