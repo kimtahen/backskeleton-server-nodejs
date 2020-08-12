d14cad01410828baa95f8c2d23d5ecabdcfa5f5c
chunghwanyoon <chunghwan@redrocket.co.kr>
2020-08-12 22:16:28 +0900

    refactor: change export method

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import IndexController from '../controllers/index.controller';
 import Route from '../interfaces/routes.interface';
 
-class IndexRoute implements Route {
+export class IndexRoute implements Route {
   public path = '/';
   public router = Router();
   public indexController = new IndexController();
@@ -15,5 +15,3 @@ class IndexRoute implements Route {
     this.router.get(`${this.path}`, this.indexController.index);
   }
 }
-
-export default IndexRoute;
